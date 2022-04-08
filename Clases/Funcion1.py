d4d7706c5fddb03a3af942b52367947bfc9cf79c
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-04-08 11:58:29 +0200

    Definimos nota

--- Clases/Funcion1.py
+++ Clases/Funcion1.py
@@ -1,2 +1,7 @@
 # Enunciado
 #Una función que reciba el fichero de calificaciones y devuelva una lista de diccionarios, donde cada diccionario contiene la información de los exámenes y la asistencia de un alumno. La lista tiene que estar ordenada por apellidos.
+
+#Empezamos definiendo la nota
+
+def nota(numero):
+    numero = numero.replace(',', '.') #Con esto lo que hacemos es cambiar la coma que separa el numero entero de los decimales por un punto.
